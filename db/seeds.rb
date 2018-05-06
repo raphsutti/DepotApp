5c74fa9b388e59452450b43df8ddaf76ede36632
raphsutti <neoster9@gmail.com>
2018-05-06 18:04:38 +1000

    additional data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,12 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
     </p>},
   image_url: 'ruby.jpg',
   price: 49.95)
+
+  Product.create!(title: 'Eloquent JavaScript 3rd Edition',
+  description:
+    %{<p>
+    This is a book about JavaScript, programming, and the wonders of the digital. You can read it online here, or get your own paperback copy of the second edition. A paper third edition is being worked on, but it isn't clear yet when it'll be available.
+    </p>},
+  image_url: 'js.jpg',
+  price: 39.95)
 # . . .
