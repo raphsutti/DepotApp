4c27b5ff38c7f8f73ba47d21475d821cec522b6c
raphsutti <neoster9@gmail.com>
2018-05-07 09:35:58 +1000

    unique title unit test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   fixtures :products
+  test "product is not valid without a unique title" do
+    product = Product.new(title:        products(:ruby).title,
+                          description:  "yyy",
+                          price:        1,
+                          image_url:    "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
 
   test "product attributes must not be blank" do
     product = Product.new
