745ae6cd07825b0467d357b70ef2e1e848626ff1
raphsutti <neoster9@gmail.com>
2018-05-06 19:30:06 +1000

    validate img extension

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
+  # validate blank
   validates :title, :description, :image_url, presence: true
+  # validate price value >=0.01
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # validate unique title
   validates :title, uniqueness: true
+  # validate image URL - ends with .gif .jpg .png
+  validates :image_url, allow_blank: true, format: {
+    with:     %r{\.(gif|jpg|png)\Z}i,
+    message:  'must be a URL for GIF, JPG or PNG image.'
+  }
 end
