ff61c6b318e09faa513446c95ea77bb8aac4a6ac
raphsutti <neoster9@gmail.com>
2018-05-07 09:45:32 +1000

    I18n function

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   fixtures :products
-  test "product is not valid without a unique title" do
+  test "product is not valid without a unique title - il8n" do
     product = Product.new(title:        products(:ruby).title,
                           description:  "yyy",
                           price:        1,
                           image_url:    "fred.gif")
     assert product.invalid?
-    assert_equal ["has already been taken"], product.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
   test "product attributes must not be blank" do
